da64e7bb37fed34e1c48000d1ba8d59fee4c2af7
MASTRIO <mastrio5772@gmail.com>
2021-07-30 18:19:11 +1000

    .gitignore doesn't work, poggers

--- cogbox/target/debug/deps/cogbox.d
+++
@@ -1,9 +0,0 @@
-C:\Users\hclark7\Documents\GitHub\Cogbox\cogbox\target\debug\deps\cogbox.exe: src\main.rs src\scripts_manager.rs src\console.rs src\console\action.rs src\console\action\script_compiler.rs
-
-C:\Users\hclark7\Documents\GitHub\Cogbox\cogbox\target\debug\deps\cogbox.d: src\main.rs src\scripts_manager.rs src\console.rs src\console\action.rs src\console\action\script_compiler.rs
-
-src\main.rs:
-src\scripts_manager.rs:
-src\console.rs:
-src\console\action.rs:
-src\console\action\script_compiler.rs:
